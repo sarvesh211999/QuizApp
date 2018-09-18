af82650ab395ab775c03f7aefac60dae9dfac0e3
sarvesh211999 <sarvesh211999@gmail.com>
2018-09-19 02:55:55 +0530

    change branch

--- go/src/main.go
+++ go/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    quiz"./quiz"
     "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
     "github.com/jinzhu/gorm"
@@ -29,9 +30,14 @@ func main() {
   
     defer db.Close()
     db.AutoMigrate(&User{})
+    db.AutoMigrate(&quiz.Quiz{})
+    db.AutoMigrate(&quiz.QuizSet{})
+    db.AutoMigrate(&quiz.Category{})
 
     r := gin.Default()
     r.POST("/signup",Register)
+    r.POST("/addCategory",AddCategory)
+    r.GET("/allCategory",GetAllCategory)
     r.Use((cors.Default()))
     r.Run(":8080")
    
@@ -41,6 +47,28 @@ func Register(c *gin.Context) {
     var user User
     c.BindJSON(&user)
     db.Create(&user)
-    c.Header("access-control-allow-origin", "*") // Why am I doing this? Find out. Try running with this line commented
+    c.Header("access-control-allow-origin", "*")
     c.JSON(200, user)
+}
+
+
+func AddCategory(c *gin.Context) {
+	var cat quiz.Category
+	c.BindJSON(&cat)
+	fmt.Println("assad")
+	db.Create(&cat)
+	c.Header("access-control-allow-origin", "*")
+	c.JSON(200,cat)
+}
+
+func GetAllCategory(c *gin.Context) {
+	var categories []quiz.Category
+	if err := db.Find(&categories).Error; err != nil {
+      c.AbortWithStatus(404)
+      fmt.Println(err)
+   } else {
+      c.Header("access-control-allow-origin", "*") // Why am I doing this? Find out. Try running with this line commented
+      c.JSON(200, categories)
+   }
+
 }
